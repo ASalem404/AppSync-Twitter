d1df93d69578c6c7e4aa7102e1ff0892409d9ea0
ASalem404 <ahmed.salem.prime@gmail.com>
2024-09-21 11:30:17 +0300

    ✨  Add lambda function for saving the new users info after signing him up

---
+++ functions/confirm-user-signup.js
@@ -0,0 +1,36 @@
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const Chance = require("chance");
+
+const DynamoDBClient = new DynamoDB.DocumentClient();
+const chance = new Chance();
+const { USER_TABLE } = process.env;
+
+module.exports.handler = async (event) => {
+  if (event.triggerSource === "PostConfirmation_ConfirmSignup") {
+    const name = event.request.userAttributes["name"];
+    const suffix = chance.string({
+      length: 8,
+      alpha: true,
+      casing: "upper",
+      numeric: "true",
+    });
+    const screenName = `${name.replace(/ [^a-zA-Z0-9]/g, "")}${suffix}`;
+    const user = {
+      id: event.userName,
+      name,
+      screenName,
+      createdAt: new Date().toJSON(),
+      followersCount: 0,
+      followingCount: 0,
+      tweetsCount: 0,
+      likesCount: 0,
+    };
+    await DynamoDBClient.put({
+      TableName: USER_TABLE,
+      Item: user,
+      ConditionExpression: "attribute_not_exists(id)",
+    }).promise();
+  }
+
+  return event;
+};
