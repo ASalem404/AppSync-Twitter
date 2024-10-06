846828f60d5d5a531d15f8033ae6295e4a526acb
ASalem404 <ahmed.salem.prime@gmail.com>
2024-10-06 03:34:07 +0300

    ✨ New function return signed url for uploading image

---
+++ functions/get-upload-url.js
@@ -0,0 +1,33 @@
+const S3 = require("aws-sdk/clients/s3");
+const s3 = new S3({ useAccelerateEndpoint: true });
+const ulid = require("ulid");
+
+const { BUCKET_NAME } = process.env;
+
+module.exports.handler = async (event) => {
+  const id = ulid.ulid();
+
+  let key = `${event.identity.username}/${id}`;
+  const extension = event.arguments.extension;
+
+  if (extension) {
+    key = extension.startWith(".")
+      ? (key += extension)
+      : (key += `.${extension}`);
+  }
+
+  const contentType = event.arguments.contentType || "image/jpeg";
+  if (!contentType.startWith("/image"))
+    throw new Error("Content Type should be an image.");
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: key,
+    ContentType: contentType,
+    ACL: "public-read",
+  };
+
+  const signedUrl = s3.getSignedUrl("putObject", params);
+
+  return signedUrl;
+};
